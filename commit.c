5ec400ccded37a3418942720556d979878e4665a
Zayaan Ahmed <zayaanahmed6506@gmail.com>
2026-04-15 22:19:14 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-// Forward declarations (implemented in object.c)
+// Forward declarations 
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out);
 
