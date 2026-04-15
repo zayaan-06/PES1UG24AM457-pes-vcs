bcc7c1254aa69d4b0312207552a2e29c7dab50d5
Zayaan Ahmed <zayaanahmed6506@gmail.com>
2026-04-15 22:27:02 +0530

    Doc-1

--- object.c
+++ object.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 
 // ─── ALL PROVIDED & IMPLEMENTED FUNCTIONS ───────────────────────────────────
-
+//initializaing hashes 
 void hash_to_hex(const ObjectID *id, char *hex_out) {
     for (int i = 0; i < HASH_SIZE; i++) sprintf(hex_out + i * 2, "%02x", id->hash[i]);
     hex_out[HASH_HEX_SIZE] = '\0';
